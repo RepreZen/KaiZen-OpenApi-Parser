823018d3e34829ece2b9d90ca5bd98b91b5c4430
Andy Lowry <andy.work@nglowry.com>
2017-09-25 12:57:27 -0400

    [#4] Use JSONAssert library for serialization tests

--- kaizen-openapi-parser/src/test/java/com/reprezen/swaggerparser/test/SimpleSerializationTest.java
+++ kaizen-openapi-parser/src/test/java/com/reprezen/swaggerparser/test/SimpleSerializationTest.java
@@ -17,14 +17,16 @@ import java.util.Deque;
 import java.util.Iterator;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
@@ -79,72 +81,11 @@ public class SimpleSerializationTest extends Assert {
 	public String fileName;
 
 	@Test
-	public void serializeExample() throws IOException {
+	public void serializeExample() throws IOException, JSONException {
 		OpenApi3 model = (OpenApi3) new OpenApiParser().parse(exampleUrl);
-		JsonNode serialized = ((OpenApi3Impl) model).createJson();
-		JsonNode expected = yamlMapper.readTree(exampleUrl);
-		assertTrue(treeEquals(expected, serialized, "[root]"));
-	}
-
-	private boolean treeEquals(JsonNode x, JsonNode y, String path) throws JsonProcessingException {
-		if ((x == null || x.isMissingNode()) != (y == null || y.isMissingNode())) {
-			return false;
-		} else if (x.getClass() != y.getClass()) {
-			return false;
-		} else {
-			switch (x.getNodeType()) {
-			case ARRAY:
-				if (x.size() != y.size()) {
-					return false;
-				}
-				for (int i = 0; i < x.size(); i++) {
-					if (!treeEquals(x.get(i), y.get(i), path + "/" + i)) {
-						System.out.printf("Trees differed at path %s/%s\n%s\n%s\n", path, i, mapper.writeValueAsString(x), mapper.writeValueAsString(y));
-						return false;
-					}
-				}
-				return true;
-			case BOOLEAN:
-				return x.asBoolean() == y.asBoolean();
-			case NULL:
-				return true;
-			case NUMBER:
-				Number xn = null, yn = null;
-				if (x.isDouble()) {
-					xn = x.doubleValue();
-					yn = y.doubleValue();
-				} else if (x.isFloat()) {
-					xn = x.floatValue();
-					yn = y.floatValue();
-				} else if (x.isInt()) {
-					xn = x.intValue();
-					yn = y.intValue();
-				} else if (x.isLong()) {
-					xn = x.longValue();
-					yn = y.longValue();
-				} else if (x.isShort()) {
-					xn = x.shortValue();
-					yn = y.shortValue();
-				}
-				return xn.equals(yn);
-			case OBJECT:
-				if (x.size() != y.size()) {
-					return false;
-				}
-				for (Iterator<String> iter = x.fieldNames(); iter.hasNext();) {
-					String propName = iter.next();
-					if (!treeEquals(x.get(propName), y.get(propName), path + "/" + propName)) {
-						System.out.printf("Trees differed at path %s/%s\n%s\n%s\n", path, propName, mapper.writeValueAsString(x), mapper.writeValueAsString(y));
-						return false;
-					}
-				}
-				return true;
-			case STRING:
-				return x.asText().equals(y.asText());
-			default:
-				return false;
-			}
-		}
+		String serialized = mapper.writeValueAsString(((OpenApi3Impl) model).createJson());
+		String expected = mapper.writeValueAsString(yamlMapper.readTree(exampleUrl));
+		JSONAssert.assertEquals(expected, serialized, JSONCompareMode.STRICT);
 	}
 
 	private static <T> Iterable<T> iterable(final Iterator<T> iterator) {

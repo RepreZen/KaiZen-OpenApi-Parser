d3eb6c13f6a7dd6e20a950ef3a8913bfe732ebcf
Andy Lowry <andy.work@nglowry.com>
2017-10-02 18:47:37 -0400

    [#91] Improved implementation of this change
    Instead of just forcing the parser to treat the model as OAS3, we also
    change the return type for all the parse methods to OpenApi3. This is
    done by creating a 3.0-locked OpenApiParser and then delegating through
    it, casting return values to OpenApi3.

--- kaizen-openapi-parser/src/main/java/com/reprezen/kaizen/oasparser/OpenApi3Parser.java
+++ kaizen-openapi-parser/src/main/java/com/reprezen/kaizen/oasparser/OpenApi3Parser.java
@@ -10,14 +10,52 @@
  *******************************************************************************/
 package com.reprezen.kaizen.oasparser;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.reprezen.kaizen.oasparser.model3.OpenApi3;
 
 public class OpenApi3Parser extends OpenApiParser {
 
-	@Override
-	protected boolean isVersion3(JsonNode tree) {
-		return true;
+	private OpenApiParser parser = new OpenApiParser() {
+
+		@Override
+		protected boolean isVersion3(JsonNode tree) {
+			return true;
+		}
+	};
+
+	public OpenApi3 parse(String spec, URL resolutionBase) {
+		return (OpenApi3) parser.parse(spec, resolutionBase);
 	}
 
-	
+	public OpenApi3 parse(String spec, URL resolutionBase, boolean validate) {
+		return (OpenApi3) parser.parse(spec, resolutionBase, validate);
+	}
+
+	public OpenApi3 parse(File specFile) {
+		return (OpenApi3) parser.parse(specFile);
+	}
+
+	public OpenApi3 parse(File specFile, boolean validate) {
+		return (OpenApi3) parser.parse(specFile, validate);
+	}
+
+	public OpenApi3 parse(URI uri) {
+		return (OpenApi3) parser.parse(uri);
+	}
+
+	public OpenApi3 parse(URI uri, boolean validate) {
+		return (OpenApi3) parser.parse(uri, validate);
+	}
+
+	public OpenApi3 parse(URL resolutionBase) {
+		return (OpenApi3) parser.parse(resolutionBase);
+	}
+
+	public OpenApi3 parse(URL resolutionBase, boolean validate) {
+		return (OpenApi3) parser.parse(resolutionBase, validate);
+	}
 }

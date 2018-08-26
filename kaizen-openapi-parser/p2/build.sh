494f2dea36231ad1cd09ddb0ee356cc392487042
Andy Lowry <andy.work@nglowry.com>
2018-08-25 20:27:50 -0400

    [#189] Add option to publish to `/latest` site

--- kaizen-openapi-parser/p2/build.sh
+++ kaizen-openapi-parser/p2/build.sh
@@ -4,11 +4,12 @@ unset JOVL_VERSION KZOP_VERSION BUILD_SCRIPT_DIR
 KZOP_REPO="http://localhost:8000"
 JOVL_REPO="http://localhost:8000"
 PUBLISH=false
+LATEST=false
 HELP=false
 
 function getargs() {
-    longopts="jovl-version:,kzop-version:,kzop-repo:,jovl-repo:,publish,build-scripts:,help"
-    shortopts="j:k:K:J:pb:h"
+    longopts="jovl-version:,kzop-version:,kzop-repo:,jovl-repo:,publish,build-scripts:,latest,help"
+    shortopts="j:k:K:J:pb:lh"
     progname=$(basename "$0")
     OPTS=$(getopt -n "$progname" -o "$shortopts" --long "$longopts" -- "$@")
     if [[ $? -ne 0 ]] ; then
@@ -29,6 +30,8 @@ function getargs() {
 		PUBLISH=true ; shift ;;
 	    -b|--build-scripts)
 		BUILD_SCRIPT_DIR="$2"; shift 2 ;;
+	    -l|--latest)
+		LATEST=true ; shift ;;
 	    -h|--help)
 		HELP=true ; shift ;;
 	    --)
@@ -42,6 +45,10 @@ function getargs() {
     fi
     if $PUBLISH && [[ -z $BUILD_SCRIPT_DIR ]] ; then
 	echo "Must specify build-script directory with -b or --build-scripts option if publishing"
+	return 1
+    fi
+    if $LATEST && ! $PUBLISH ; then
+	echo "Must specify --publish if --latest is used"
 	return 1
     fi
     if [[ -z $JOVL_VERSION || -z $KZOP_VERSION ]] ; then
@@ -72,8 +79,10 @@ function usage() {
 	  -p or --publish - publish the newly built update site
 	* -b or --build-scripts - speicfy location of RepreZen build scripts, including 
 	     the "publish" script. This is required only if "-p" is specified
+	  -l or --latest - publish to "latest" site in addition to site named for 
+	     version (--publish must also be specified) 
 	  -h or --help - print this message and do nothing else
-
+	
 	Note on repos: Both ---kzop-repo and --jovl-repo default to
 	http://localhost:8000, so if you already have the needed versions in
 	your local maven repo cache, you can set up a simple HTTP server to
@@ -93,6 +102,9 @@ function main() {
     buildSite
     if $PUBLISH ; then
 	publishSite
+	if $LATEST ; then
+	    publishLatest
+	fi
     fi
 }
 
@@ -110,14 +122,23 @@ function buildSite() {
 }
 
 function publishSite() {
+    publish "$KZOP_VERSION"
+}
+
+function publishLatest() {
+    publish "latest"
+}
+
+function publish() {
     local pub="$BUILD_SCRIPT_DIR/publish"
-    "$pub" -w -c -d "$KZOP_VERSION" \
+    "$pub" -w -c -d "$1" \
 	   --ws-host wsw \
 	   --ws-root /data/products/kaizen/openapi-parser/updates \
 	   --local-root target/repository \
 	   \*
 		       
 }
+
 if ! getargs "$@" ; then
     usage
     exit 1
